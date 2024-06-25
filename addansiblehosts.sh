90d57c2d4e7ae25e5b841d01a0cea820aa79f43b
amgad96 <43969372+amgad96@users.noreply.github.com>
2024-06-25 06:40:50 +0300

    Update for aws addansiblehosts.sh

--- addansiblehosts.sh
+++ addansiblehosts.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-# This command to generate the ssh key and copy it to hosts 
+# This command to generate the ssh key and copy it to hosts
 # Note : These IPs are just for testing, and it will be changed. 
-ips=("161.35.219.190" "134.122.78.18")
+ips=("54.196.61.7")
 
 # This command to generate the ssh key and automatically enter key#
-echo -ne '\n' | ssh-keygen -t rsa -b 4096 
+echo -ne '\n' | ssh-keygen -t rsa -b 4096
 #echo -ne '\n' | ssh-keygen -t rsa -b 4096 -y
 # This loop to copy the ssh key to all hosts ips
 for remote_server_ip in ${ips[@]};do
   echo "Copying SSH key to $remote_server_ip"
   # Use ssh option [-o StrictHostKeyChecking=yes] to automatically accept the host key
-  ssh-copy-id  root@$remote_server_ip 
-  #ssh-copy-id -o StrictHostKeyChecking=no root@$remote_server_ip 
+  # This command to copy the 
+  cat ~/.ssh/id_rsa.pub | ssh -i /home/ubuntu/KubeBS.pem ubuntu@$remote_server_ip 'mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys'
+  ###ssh-copy-id  root@$remote_server_ip
+  #ssh-copy-id -o StrictHostKeyChecking=no root@$remote_server_ip
 
 done
