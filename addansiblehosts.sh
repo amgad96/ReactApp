8dbbbfdf8f9a8add385c1d2b2a039aa5a6ee630e
amgad96 <43969372+amgad96@users.noreply.github.com>
2024-06-29 23:06:48 +0300

    Update addansiblehosts.sh
    add    -o StrictHostKeyChecking=no

--- addansiblehosts.sh
+++ addansiblehosts.sh
@@ -10,8 +10,9 @@ echo -ne '\n' | ssh-keygen -t rsa -b 4096
 for remote_server_ip in ${ips[@]};do
   echo "Copying SSH key to $remote_server_ip"
   # Use ssh option [-o StrictHostKeyChecking=yes] to automatically accept the host key
-  # This command to copy the 
-  cat ~/.ssh/id_rsa.pub | ssh -i /home/ubuntu/KubeBS.pem ubuntu@$remote_server_ip 'mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys'
+  # This command to copy the ssh key to worker nodes
+  cat ~/.ssh/id_rsa.pub | ssh -o StrictHostKeyChecking=no -i /home/ubuntu/KubeBS.pem ubuntu@$remote_server_ip 'mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys'
+  #cat ~/.ssh/id_rsa.pub | ssh -i /home/ubuntu/KubeBS.pem ubuntu@$remote_server_ip 'mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys'
   ###ssh-copy-id  root@$remote_server_ip
   #ssh-copy-id -o StrictHostKeyChecking=no root@$remote_server_ip
 
